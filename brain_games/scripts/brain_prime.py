fa37b250ba08b638b61d474a839a98ee31e180c4
Швецов Юрий Валентинович <Shvetsov.Y.Val@sberbank.ru>
2023-05-14 20:21:43 +0300

    fix prime

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -7,7 +7,7 @@ ANSWERS_TO_WIN = 3
 def is_prime(n: int) -> bool:
     if n < 2:
         return False
-    for i in range(1, n):
+    for i in range(2, n):
         if n % i == 0:
             return False
     return True
