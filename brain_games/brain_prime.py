175b1935665e5750d1797516864bceb49829013b
Швецов Юрий Валентинович <Shvetsov.Y.Val@sberbank.ru>
2023-05-14 20:10:14 +0300

    fix descr

--- brain_games/brain_prime.py
+++ brain_games/brain_prime.py
@@ -17,7 +17,7 @@ def main():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
 
     right_answers = 0
     while True:
